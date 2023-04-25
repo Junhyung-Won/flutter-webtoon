126538723d2149ed67bfcf8a01a71db2e0cb34fa
Junhyung Won <junhyung.w@gmail.com>
2023-04-25 19:49:19 -0400

    Episode and Detail model, api Appended

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:webtoon/models/webtoon.dart';
+import 'package:webtoon/models/webtoon_episode_model.dart';
 
 class ApiService {
   static const String baseUrl =
@@ -19,6 +20,31 @@ class ApiService {
         webtoonInstances.add(instance);
       }
       return webtoonInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonEpisodeModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonEpisodeModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+      String id) async {
+    List<WebtoonEpisodeModel> episodesInstances = [];
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesInstances;
     }
     throw Error();
   }
